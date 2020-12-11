49cdd0bb8e1d48828701a64a8ace4fcda9ccbc52
xzc <377608789@qq.com>
2020-12-11 13:54:00 +0800

    feat:第N天学习

--- src/main/java/com/xzc/tabletest/TableTestKafkaPipeLine.java
+++ src/main/java/com/xzc/tabletest/TableTestKafkaPipeLine.java
@@ -2,6 +2,7 @@ package com.xzc.tabletest;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.StatementSet;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
@@ -23,7 +24,7 @@ public class TableTestKafkaPipeLine {
                 ") WITH (" +
                 " 'connector' = 'kafka'," +
                 " 'topic' = 'test'," +
-                " 'properties.bootstrap.servers' = '101.200.43.65:9092,47.95.29.151:9092,101.200.216.136:9092'," +
+                " 'properties.bootstrap.servers' = ''," +
                 " 'properties.group.id' = 'testGroup'," +
                 " 'format' = 'csv'," +
                 " 'scan.startup.mode' = 'earliest-offset'" +
@@ -32,19 +33,35 @@ public class TableTestKafkaPipeLine {
 
         Table kafkaTable = streamTableEnvironment.from("kafkaTable");
 
-        Table table = streamTableEnvironment.sqlQuery("select * from kafkaTable");
-        streamTableEnvironment.toAppendStream(table, Row.class).print();
-
-        Table filter = streamTableEnvironment.sqlQuery("select id, temperature from kafkaTable").filter($("id").isEqual("sensor_1"));
-        streamTableEnvironment.toAppendStream(filter, Row.class).print("过滤1");
-        Table filter2 = streamTableEnvironment.sqlQuery("select id, temperature from kafkaTable where id = 'sensor_1'");
-        streamTableEnvironment.toAppendStream(filter2, Row.class).print("过滤2");
-        // 聚合统计
-        Table select = kafkaTable.groupBy($("id")).select($("id"), $("temperature").sum().as("sum"));
-        streamTableEnvironment.toRetractStream(select, Row.class).print("聚合统计11");
+//        Table table = streamTableEnvironment.sqlQuery("select * from kafkaTable");
+//        streamTableEnvironment.toAppendStream(table, Row.class).print();
+//
+//        Table filter = streamTableEnvironment.sqlQuery("select id, temperature from kafkaTable").filter($("id").isEqual("sensor_1"));
+//        streamTableEnvironment.toAppendStream(filter, Row.class).print("过滤1");
+//        Table filter2 = streamTableEnvironment.sqlQuery("select id, temperature from kafkaTable where id = 'sensor_1'");
+//        streamTableEnvironment.toAppendStream(filter2, Row.class).print("过滤2");
+//        // 聚合统计
+//        Table select = kafkaTable.groupBy($("id")).select($("id"), $("temperature").sum().as("sum"));
+//        streamTableEnvironment.toRetractStream(select, Row.class).print("聚合统计11");
         Table groupTable = streamTableEnvironment.sqlQuery("select id, sum(temperature) from kafkaTable group by id");
-        streamTableEnvironment.toRetractStream(groupTable, Row.class).print("聚合统计222");
+//        streamTableEnvironment.toRetractStream(groupTable, Row.class).print("聚合统计222");
 
-        env.execute("kafka table");
+        String mysqlConnection = " create table sersor " +
+                "( id String, " +
+                " times String, " +
+                " temp Double, " +
+                " PRIMARY KEY (id,times) NOT ENFORCED " +
+                " ) with ( " +
+                " 'connector' = 'jdbc', " +
+                " 'url' = 'jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=UTF-8&serverTimezone=GMT%2B8&autoReconnect=true&failOverReadOnly=false', " +
+                " 'username' = 'root', " +
+                " 'password' = '123456', " +
+                " 'driver' = 'com.mysql.cj.jdbc.Driver', " +
+                " 'table-name' = 'sersor_temp' )";
+        streamTableEnvironment.executeSql(mysqlConnection);
+
+        streamTableEnvironment.executeSql("insert into sersor select id, times, sum(temperature) from kafkaTable group by id,times");
+
+//        env.execute("kafka table");
     }
 }
